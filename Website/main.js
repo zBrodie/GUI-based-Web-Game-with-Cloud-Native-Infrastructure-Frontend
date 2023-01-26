7a75b12c805edcd1736f551ad186e9f0786368a3
Justin Neigh <zimiyi00@gmail.com>
2023-01-25 20:19:38 -0500

    Bug Fixes

--- Website/main.js
+++ Website/main.js
@@ -1,17 +1,11 @@
-//const express = require("express");
-//const path = require("path");
-import ReactDOM from "react-dom";
-import React from "react";
 
-const App = () => {
-        return (<h1>This is my React app!</h1>);
-}
-ReactDOM.render(<App />, document.getElementById("app"));
-/*
+const express = require("express");
+const path = require("path");
+
 const app = express();
 
 app.get("/", (req, res) => {
-        res.sendFile( __dirname+"/Templates/Login.html");
+        res.sendFile( __dirname+"/Templates/index.html");
 });
 app.get("/Login", (req, res) => {
         res.sendFile( __dirname+"/Templates/Login.html");
@@ -25,4 +19,3 @@ app.get("/home", (req, res) => {
 
 
 app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
-*/
