4eb43a41f09a203684911be589c8db8f5903782a
jnaholnik08 <jnaholnik@gmail.com>
2023-01-30 23:39:14 -0500

    SignupPage.js created, App.js tinkered, audio in works

--- upwardmobility/src/App.js
+++ upwardmobility/src/App.js
@@ -1,56 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import './LoginAndSignupPage.css';
 import unknown from './unknown@2x.png';
 import background from './sayonara4now-cyberpunk-city-sc.png';
 import upward from './Upward_Mobility_big.png'
 import { useNavigate} from "react-router-dom";
+import logo from './Upward_Mobility_big_x.png'
 
 function App() {
   return (
     <div className="App">
-      <div id = "HomePage">
-        <img id= "sayonara4now-cyberpunk-city-sc_k" src="{background}" srcset="sayonara4now-cyberpunk-city-sc_k.png 1x, sayonara4now-cyberpunk-city-sc_k@2x.png 2x"/>
-        <div className="joinGameButton">
-          <button name="" type="submit" id="joinGameButton" rx="0" ry="0" x="0" y="0" width="264" height="83">JOIN
-            GAME
-          </button>
-        </div>
+      <div id="Web_1920__2">
+        <img id="sayonara4now-cyberpunk-city-sc" src="sayonara4now-cyberpunk-city-sc.png"
+             srcSet="sayonara4now-cyberpunk-city-sc.png 1x, sayonara4now-cyberpunk-city-sc@2x.png 2x"/>
 
-        <div className="createGameButton">
-          <button type="submit" id="createGameButton" rx="0" ry="0" x="0" y="0" width="264" height="83">CREATE GAME
-          </button>
-        </div>
+          <svg className="Ellipse_1">
+            <ellipse id="Ellipse_1" rx="42" ry="47" cx="42" cy="47">
+            </ellipse>
+          </svg>
+          <svg className="Rectangle_30">
+            <rect id="Rectangle_30" rx="0" ry="0" x="0" y="0">
+            </rect>
+          </svg>
+          <svg className="Rectangle_34">
+            <rect id="Rectangle_34" rx="0" ry="0" x="0" y="0" width="1" height="1">
+            </rect>
+          </svg>
+          <div id="Please_Log_In">
+            <span>Please Log In:</span>
+          </div>
+          <svg className="Username_Input_Field">
+            <input type="text"
+                   placeholder="USERNAME"
+                   id="Username_Input_Field" rx="0" ry="0" x="0" y="0">
+            </input>
+          </svg>
+          <svg className="Password_Input_Field">
+            <input type="text"
+                   placeholder="PASSWORD"
+                   id="Password_Input_Field" rx="0" ry="0" x="0" y="0">
+            </input>
+          </svg>
 
-        <svg className="Ellipse_1">
-          <ellipse id="Ellipse_1" rx="42" ry="47" cx="42" cy="47">
-          </ellipse>
-        </svg>
+          <div id="Dont_have_an_account__Click_He">
+            <a href="javascript:alert('Hello World!');">Dont have an account?<br/><br/>Click Here</a>
+          </div>
+          <div id="Forgot_Password">
+            <a href="">Forgot Password?</a>
+          </div>
+          <img id="Upward_Mobility_big" src="{upward}"
+               srcSet="Upward_Mobility_big.png 1x, Upward_Mobility_big@2x.png 2x"/>
 
-        <img id ="Upward_Mobility_big" src="{upward}" srcset="Upward_Mobility_big.png 1x, Upward_Mobility_big@2x.png 2x" />
+            <img id="Upward_Mobility_big_x" src="{logo}"
+                 srcSet="Upward_Mobility_big_x.png 1x, Upward_Mobility_big_x@2x.png 2x"/>
+
+              <svg className="Login_Button">
+                <button id="Login_Button" rx="0" ry="0" x="0" y="0">
+                </button>
+              </svg>
+        <script>
+          var audio = new Audio('');
+          audio.play;
+        </script>
       </div>
-
-      <div className="sideMenu">
-        <rect className="sideMenu"></rect>
-        <img id="unknown" src="{unknown}" srcset="unknown.png 1x, unknown@2x.png 2x"  />
-        <div id="accountName">
-          <span>THELEGEND27</span>
-        </div>
-
-        <div className="statsButton">
-          <button id="statsButton" rx="0" ry="0" x="0" y="0" width="260" height="62">STATS</button>
-        </div>
-
-        <div className="achievementsButton">
-          <button id="achievementsButton" rx="0" ry="0" x="0" y="0" width="260" height="62" top="376">ACHIEVEMENTS
-          </button>
-        </div>
-
-        <div className="settingsButton">
-          <button id="settingsButton" rx="0" ry="0" x="0" y="0" width="260" height="62">SETTINGS</button>
-        </div>
-
-      </div>
-
     </div>
   );
 }
