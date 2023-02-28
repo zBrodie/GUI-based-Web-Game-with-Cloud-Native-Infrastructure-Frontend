c316012d47ea8d5fe2fbb0ca850e69fe2a5bc641
jnaholnik08 <jnaholnik@gmail.com>
2023-02-28 14:33:13 -0500

    GamePage js and css file reformatting

--- upwardmobility/src/GamePage.js
+++ upwardmobility/src/GamePage.js
@@ -46,38 +46,40 @@ function GamePage(){
                 <span>A pair of strange dice lay before you...</span>
             </div>
 
-            <svg className="Rectangle_46">
-                <rect id="Rectangle_46" rx="0" ry="0" x="0" y="0" width="996" height="146">
+
+            {/*Player List*/}
+            <svg className="GamePlayerBackground">
+                <rect id="GamePlayerBackground" rx="0" ry="0" x="0" y="0" width="996" height="146">
                 </rect>
             </svg>
-
             <img id="NoPath_-_Copy_10" src="NoPath_-_Copy_10.png" srcSet="NoPath_-_Copy_10.png 1x, NoPath_-_Copy_10@2x.png 2x"/>
 
-            <svg className="Ellipse_2">
-                <ellipse id="Ellipse_2" rx="53" ry="53" cx="53" cy="53">
+            <svg className="GamePlayer_1">
+                <ellipse id="GamePlayer_1" rx="53" ry="53" cx="53" cy="53">
                 </ellipse>
             </svg>
-            <svg className="Ellipse_3">
-                <ellipse id="Ellipse_3" rx="53" ry="53" cx="53" cy="53">
+            <svg className="GamePlayer_2">
+                <ellipse id="GamePlayer_2" rx="53" ry="53" cx="53" cy="53">
                 </ellipse>
             </svg>
-            <svg className="Ellipse_4">
-                <ellipse id="Ellipse_4" rx="53" ry="53" cx="53" cy="53">
+            <svg className="GamePlayer_3">
+                <ellipse id="GamePlayer_3" rx="53" ry="53" cx="53" cy="53">
                 </ellipse>
             </svg>
-            <svg className="Ellipse_5">
-                <ellipse id="Ellipse_5" rx="53" ry="53" cx="53" cy="53">
+            <svg className="GamePlayer_4">
+                <ellipse id="GamePlayer_4" rx="53" ry="53" cx="53" cy="53">
                 </ellipse>
             </svg>
-            <svg className="Ellipse_6">
-                <ellipse id="Ellipse_6" rx="53" ry="53" cx="53" cy="53">
+            <svg className="GamePlayer_5">
+                <ellipse id="GamePlayer_5" rx="53" ry="53" cx="53" cy="53">
                 </ellipse>
             </svg>
-            <svg className="Ellipse_7">
-                <ellipse id="Ellipse_7" rx="53" ry="53" cx="53" cy="53">
+            <svg className="GamePlayer_6">
+                <ellipse id="GamePlayer_6" rx="53" ry="53" cx="53" cy="53">
                 </ellipse>
             </svg>
 
+            {/*Inventory List*/}
             <svg className="InventoryLabel">
                 <rect id="InventoryLabel" rx="0" ry="0" x="0" y="0">
                 </rect>
@@ -86,14 +88,16 @@ function GamePage(){
             <div id="Inventory_Label_Word">
                 <span>Inventory</span><br/>
             </div>
+
+            {/*Chat Menu*/}
             <input className="chatBox" type="text" placeholder="Type here to chat" id="chatBox" rx="7" ry="7" x="0" y="0" width="290" height="33"/>
 
             <div id="Type_here_to_chat">
                 <span>Type here to chat...</span>
             </div>
 
-            <svg className="Rectangle_49">
-                <rect id="Rectangle_49" rx="0" ry="0" x="0" y="0"></rect>
+            <svg className="GamePageIcon">
+                <rect id="GamePageIcon" rx="0" ry="0" x="0" y="0"></rect>
             </svg>
             <img id="Upward_Mobility_big_Game_Page" src="Upward_Mobility_big.png" srcSet="Upward_Mobility_big.png 1x, Upward_Mobility_big@2x.png 2x"/>
 
